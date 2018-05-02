a2dca1c8e3f01c6e205794600bcc6eeb81d20fc0
Christian Aranda <christian@wearemobilefirst.com>
2018-05-02 15:50:55 +0200

    Add status controller

---
+++ ARNotes/Controllers/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  ARNotes
+//
+//  Created by Christian Aranda on 02/05/2018.
+//  Copyright © 2018 We Are Mobile First. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    private weak var statusViewController: StatusViewController!
+
+    /// Prevents restarting the session while a restart is in progress.
+    var isRestartAvailable = true
+    
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        
+        self.statusViewController = self.childViewControllers
+            .compactMap { $0 as? StatusViewController }.first
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        resetTracking()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        session.pause()
+    }
+    
+    /// Creates a new AR configuration to run on the `session`.
+    func resetTracking() {
+        
+        let configuration = ARWorldTrackingConfiguration()
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        statusViewController.scheduleMessage("Look around to detect images", inSeconds: 7.5, messageType: .contentPlacement)
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    
+    // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        statusViewController.showTrackingQualityInfo(for: camera.trackingState, autoHide: true)
+        
+        switch camera.trackingState {
+        case .notAvailable, .limited:
+            statusViewController.escalateFeedback(for: camera.trackingState, inSeconds: 3.0)
+        case .normal:
+            statusViewController.cancelScheduledMessage(for: .trackingStateEscalation)
+        }
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        
+        // Use `flatMap(_:)` to remove optional error messages.
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        blurView.isHidden = false
+        statusViewController.showMessage("""
+        SESSION INTERRUPTED
+        The session will be reset after the interruption has ended.
+        """, autoHide: false)
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        blurView.isHidden = true
+        statusViewController.showMessage("RESETTING SESSION")
+        
+        restartExperience()
+    }
+    
+    func sessionShouldAttemptRelocalization(_ session: ARSession) -> Bool {
+        return true
+    }
+    
+    // MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Blur the background.
+        blurView.isHidden = false
+        
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.blurView.isHidden = true
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Interface Actions
+    
+    func restartExperience() {
+        guard isRestartAvailable else { return }
+        isRestartAvailable = false
+        
+        statusViewController.cancelAllScheduledMessages()
+        
+        resetTracking()
+        
+        // Disable restart for a while in order to give the session time to restart.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.isRestartAvailable = true
+        }
+    }
+}
